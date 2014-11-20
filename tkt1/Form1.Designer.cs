0771a9ee91a23585aa04c2e3d9eee73f31e2f5f1
ironlee <litao690@163.com>
2014-11-20 15:37:42 +0800

    fir

---
+++ tkt1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace tkt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(13, 13);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(953, 346);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.glControl1_KeyDown);
+            this.glControl1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseDown);
+            this.glControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.glControl1_MouseMove);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 426);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(228, 457);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 505);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ tkt1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+namespace tkt1
+{
+    public partial class Form1 : Form
+    {
+        bool loaded = false;
+        Point mouse;
+        Point ptmp;
+        float xx = 0;
+        float yy = 0;
+        int vw = 300;
+        int vh = 300;
+        public Form1()
+        {
+            InitializeComponent();
+            glControl1.MouseWheel += glControl1_MouseWheel;
+        }
+
+        void glControl1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            vw += e.Delta;
+            vh += e.Delta;
+            glControl1.Invalidate();
+        }
+
+        private void SetupViewport()
+        {
+            GL.MatrixMode(MatrixMode.Projection); 
+            GL.LoadIdentity();
+
+            GL.Ortho(0, 400, 0, 400, -200, 200);
+            GL.Viewport(0, 0,vw, vh);
+        }
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            GL.ClearColor(Color.White);
+            SetupViewport();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded)
+            {
+                return;
+            }
+            SetupViewport();
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.Translate(200, 200, 100);
+            //draw the Coordinate system 
+            GL.Rotate(xx, OpenTK.Vector3d.UnitX);
+            GL.Rotate(yy, OpenTK.Vector3d.UnitY);
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex2(0, 0);
+            GL.Vertex2(400, 0);
+            GL.Color3(Color.Blue);
+            GL.Vertex2(0, 0);
+            GL.Vertex3(0, 400,0);
+            GL.Color3(Color.Green);
+           GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 800);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Color3(1.0f, 0.0f, 0.0f);
+            GL.Vertex3(10, 20, 0);
+            GL.Color3(0.0f, 1.0f, 0.0f);
+            GL.Vertex3(100, 20, 50);
+            GL.Color3(0.0f, 0.0f, 1.0f);
+            GL.Vertex3(100, 50, 50);
+            GL.End();
+            //GL.Rotate(rtti, OpenTK.Vector3d.UnitY);
+            //GL.Begin(PrimitiveType.Triangles);
+            //GL.Color3(1.0f, 0.0f, 0.0f);
+            //GL.Vertex3(0.0f, 20.0f, 0.0f);
+            //GL.Color3(0.0f, 1.0f, 0.0f);
+            //GL.Vertex3(-20.0f, -20.0f, 20.0f);
+            //GL.Color3(0.0f, 0.0f, 1.0f);
+            //GL.Vertex3(20.0f, -20.0f, 20.0f);
+            //GL.End();
+            GL.LoadIdentity();
+            glControl1.SwapBuffers();
+            label1.Text = xx.ToString();
+            label2.Text = yy.ToString();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded)
+            {
+                return;
+            }
+            glControl1.Invalidate();
+        }
+
+        private void glControl1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S)
+            {
+                xx += 1f;
+            }
+            if (e.KeyCode == Keys.W)
+            { 
+                xx -= 1f;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                yy += 1f;
+            }
+            if ( e.KeyCode == Keys.A)
+            {
+                yy -= 1f;
+            }
+            glControl1.Invalidate();
+        }
+
+        private void glControl1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = e.Location;
+            label3.Text = mouse.ToString();
+        }
+
+        private void glControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mnow = e.Location;
+               
+
+                label4.Text = mnow.ToString();
+                if (ptmp.Y < mnow.Y)
+                {
+                    xx += 1f;
+                }
+                else if (ptmp.Y > mnow.Y)
+                {
+                    xx -= 1f;
+                }
+                if (ptmp.X < mnow.X)
+                {
+                    yy += 1f;
+                }
+                else if (ptmp.X > mnow.X)
+                {
+                    yy -= 1f;
+                }
+                yy %= 360;
+                xx %= 360;
+                glControl1.Invalidate();
+                ptmp = e.Location;
+            }
+            label5.Text = e.Location.ToString();
+        }
+    }
+}
